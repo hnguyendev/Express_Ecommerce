9a89cda67dee577a329b54d6832ed990dff41b6c
hnguyendev <duchung682003@gmail.com>
2024-09-05 00:20:35 +0700

    Add geolocation index & optimise pagination

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -4,6 +4,8 @@ import RestaurantModel from "../models/restaurant.model";
 import { Utils } from "../utils/Utils";
 import UserModel from "../models/user.model";
 import CategoryModel from "../models/category.model";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 export class RestaurantController {
   static async createRestaurant(
@@ -57,11 +59,10 @@ export class RestaurantController {
     }
   }
 
+  //admin
   static async getRestaurants(req: Request, res: Response, next: NextFunction) {
     try {
-      const restaurants = await RestaurantModel.find({
-        status: "active",
-      });
+      const restaurants = await RestaurantModel.find({});
 
       res.status(200).json({
         success: true,
@@ -72,28 +73,66 @@ export class RestaurantController {
     }
   }
 
+  // user
   static async getNearbyRestaurants(
     req: Request,
     res: Response,
     next: NextFunction
   ) {
-    const { lat, lng, radius }: any = req.query;
+    const { lat, lng, radius, sort } = req.query;
+    const limit = 10;
+    const { cursor } = req.body;
     try {
-      const restaurants = await RestaurantModel.find({
-        status: "active",
-        location: {
-          $geoWithin: {
-            $centerSphere: [
-              [parseFloat(lng), parseFloat(lat)],
-              parseFloat(radius) / 6378.1,
-            ],
+      const sortBy: any = { _id: 1 };
+      let query: Record<string, any> = { status: "active" };
+
+      if (sort) {
+        const [value, order] = sort.toString().split("-");
+        const allowedFields = ["price", "rating"];
+        if (allowedFields.includes(value)) {
+          sortBy[value] = order === "desc" ? -1 : 1;
+        }
+      }
+
+      if (cursor) {
+        const decrypted = jwt.verify(cursor, process.env.ACCESS_TOKEN);
+        console.log(cursor);
+        query._id = { $gt: new mongoose.Types.ObjectId(decrypted as string) };
+      }
+
+      const restaurants = await RestaurantModel.aggregate([
+        {
+          $geoNear: {
+            near: {
+              type: "Point",
+              coordinates: [
+                parseFloat(lng as string),
+                parseFloat(lat as string),
+              ],
+            },
+            spherical: true,
+            query: query,
+            distanceField: "calc_distance",
+            maxDistance: parseFloat(radius as string) * 1000,
           },
         },
-      });
+        { $sort: sortBy },
+      ]).limit(limit);
+
+      let newCursor = null;
+      if (restaurants.length === limit) {
+        newCursor = jwt.sign(
+          restaurants[restaurants.length - 1]["_id"].toString(),
+          process.env.ACCESS_TOKEN
+        );
+      }
 
       res.status(200).json({
         success: true,
         restaurants,
+        pagination: {
+          cursor: newCursor,
+        },
       });
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 500));
@@ -105,24 +144,71 @@ export class RestaurantController {
     res: Response,
     next: NextFunction
   ) {
-    const { name, lat, lng, radius }: any = req.query;
+    const { name, lat, lng, radius, page = 1 } = req.query;
+    const limit = 10;
+    const skip = (parseInt(page as string, 10) - 1) * limit;
     try {
-      const restaurants = await RestaurantModel.find({
+      const total = await RestaurantModel.countDocuments({
         status: "active",
         name: { $regex: name, $options: "i" },
         location: {
           $geoWithin: {
             $centerSphere: [
-              [parseFloat(lng), parseFloat(lat)],
-              parseFloat(radius) / 6378.1,
+              [parseFloat(lng as string), parseFloat(lat as string)],
+              parseFloat(radius as string) / 6378.1,
             ],
           },
         },
       });
 
+      if (!total) {
+        return res.status(200).json({
+          success: true,
+          restaurants: [],
+        });
+      }
+
+      const pageCount = Math.ceil(total / limit);
+
+      if (
+        parseInt(page as string) > pageCount ||
+        parseInt(page as string) < 1
+      ) {
+        return next(new ErrorHandler("Invalid page number", 400));
+      }
+
+      const restaurants = await RestaurantModel.aggregate([
+        {
+          $geoNear: {
+            near: {
+              type: "Point",
+              coordinates: [
+                parseFloat(lng as string),
+                parseFloat(lat as string),
+              ],
+            },
+            spherical: true,
+            query: {
+              status: "active",
+              name: { $regex: name, $options: "i" },
+            },
+            distanceField: "calc_distance",
+            maxDistance: parseFloat(radius as string) * 1000,
+          },
+        },
+      ])
+        .skip(skip)
+        .limit(limit);
+
       res.status(200).json({
         success: true,
         restaurants,
+        pagination: {
+          page,
+          pageSize: limit,
+          total,
+          pageCount,
+        },
       });
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 500));
